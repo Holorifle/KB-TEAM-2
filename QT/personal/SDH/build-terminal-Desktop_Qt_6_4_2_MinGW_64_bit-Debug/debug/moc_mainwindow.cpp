8110bc0de783de4d5ba3a6d463fe9490ec57b923
Holorifle <shinemper@gmail.com>
2023-03-13 12:32:55 +0900

    코드수정

---
+++ QT/personal/SDH/build-terminal-Desktop_Qt_6_4_2_MinGW_64_bit-Debug/debug/moc_mainwindow.cpp
@@ -0,0 +1,179 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.4.2)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../../terminal/mainwindow.h"
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.4.2. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+#ifndef Q_CONSTINIT
+#define Q_CONSTINIT
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+namespace {
+struct qt_meta_stringdata_MainWindow_t {
+    uint offsetsAndSizes[22];
+    char stringdata0[11];
+    char stringdata1[15];
+    char stringdata2[1];
+    char stringdata3[16];
+    char stringdata4[6];
+    char stringdata5[10];
+    char stringdata6[5];
+    char stringdata7[9];
+    char stringdata8[12];
+    char stringdata9[29];
+    char stringdata10[6];
+};
+#define QT_MOC_LITERAL(ofs, len) \
+    uint(sizeof(qt_meta_stringdata_MainWindow_t::offsetsAndSizes) + ofs), len 
+Q_CONSTINIT static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+        QT_MOC_LITERAL(0, 10),  // "MainWindow"
+        QT_MOC_LITERAL(11, 14),  // "openSerialPort"
+        QT_MOC_LITERAL(26, 0),  // ""
+        QT_MOC_LITERAL(27, 15),  // "closeSerialPort"
+        QT_MOC_LITERAL(43, 5),  // "about"
+        QT_MOC_LITERAL(49, 9),  // "writeData"
+        QT_MOC_LITERAL(59, 4),  // "data"
+        QT_MOC_LITERAL(64, 8),  // "readData"
+        QT_MOC_LITERAL(73, 11),  // "handleError"
+        QT_MOC_LITERAL(85, 28),  // "QSerialPort::SerialPortError"
+        QT_MOC_LITERAL(114, 5)   // "error"
+    },
+    "MainWindow",
+    "openSerialPort",
+    "",
+    "closeSerialPort",
+    "about",
+    "writeData",
+    "data",
+    "readData",
+    "handleError",
+    "QSerialPort::SerialPortError",
+    "error"
+};
+#undef QT_MOC_LITERAL
+} // unnamed namespace
+
+Q_CONSTINIT static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+      10,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       6,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,   50,    2, 0x08,    1 /* Private */,
+       3,    0,   51,    2, 0x08,    2 /* Private */,
+       4,    0,   52,    2, 0x08,    3 /* Private */,
+       5,    1,   53,    2, 0x08,    4 /* Private */,
+       7,    0,   56,    2, 0x08,    6 /* Private */,
+       8,    1,   57,    2, 0x08,    7 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QByteArray,    6,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 9,   10,
+
+       0        // eod
+};
+
+Q_CONSTINIT const QMetaObject MainWindow::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_MainWindow.offsetsAndSizes,
+    qt_meta_data_MainWindow,
+    qt_static_metacall,
+    nullptr,
+    qt_incomplete_metaTypeArray<qt_meta_stringdata_MainWindow_t,
+        // Q_OBJECT / Q_GADGET
+        QtPrivate::TypeAndForceComplete<MainWindow, std::true_type>,
+        // method 'openSerialPort'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'closeSerialPort'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'about'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'writeData'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<const QByteArray &, std::false_type>,
+        // method 'readData'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'handleError'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<QSerialPort::SerialPortError, std::false_type>
+    >,
+    nullptr
+} };
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MainWindow *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->openSerialPort(); break;
+        case 1: _t->closeSerialPort(); break;
+        case 2: _t->about(); break;
+        case 3: _t->writeData((*reinterpret_cast< std::add_pointer_t<QByteArray>>(_a[1]))); break;
+        case 4: _t->readData(); break;
+        case 5: _t->handleError((*reinterpret_cast< std::add_pointer_t<QSerialPort::SerialPortError>>(_a[1]))); break;
+        default: ;
+        }
+    }
+}
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 6)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 6;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 6)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 6;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
